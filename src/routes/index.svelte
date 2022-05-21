832b4b2e1877c3e9780e4842c7089822b1695e4c
sekharsa <sekharsais999@gmail.com>
2022-05-21 15:28:06 +0530

    changed css of index page part-1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,17 @@
 <style>
+
+.div0 {
+	background-color: rgb(66, 21, 191);
+	height: 170px;
+	width: 100%;
+	margin: 2px 20px 20px 20px;
+	box-shadow: 0 3px 10px rgba(255, 255, 255, 0.75);
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+}
+
 .div1 {
 	background-color: #ec4141;
 	border-radius: 5px;
@@ -8,7 +21,7 @@
 }
 
 .div2 {
-	background-color: #f5f5f5;
+	background-color: #0b2477;
 	border-radius: 5px;
 	border-color: gold;
 	padding: 20px;
@@ -20,14 +33,16 @@
 	border-radius: 5px;
 	padding: 20px;
 	margin-bottom: 20px;
-	/* border: 5px solid rgb(240, 223, 68); */
+	border: 5px solid rgb(240, 223, 68);
 	margin: 2px;
+	
 }
 .div4 {
-	background-color: #f5f5f5;
+	background-color: #240be2;
 	border-radius: 5px;
 	padding: 20px;
 	margin-bottom: 20px;
+	height: 130px;
 }
 </style>
 
@@ -48,17 +63,25 @@ function selectedMovie(movie) {
 }
 </script>
 
-<h1>MOVIES</h1>
+<div class="div0">
+	<h1 class="text-8xl"><b>MOVIES</b></h1>
+</div>
+
+
 
 <div class="div1">
 	<div class="div2 grid grid-flow-row grid-cols-2">
 		{#each data as d}
 			{#each d.movies as m}
 				<div class="div3">
-					<a href="{`/seat-layout?movieName=${m.name}&price=${m.cost}`}"><h2>{m.name}</h2></a>
-					<p>{m.cost}</p>
+					<a href="{`/seat-layout?movieName=${m.name}&price=${m.cost}`}"><h2 class="text-white text-2xl text-center">{m.name}</h2></a>
+					<p class="text-white text-2xl text-center">{m.cost}</p>
 				</div>
 			{/each}
 		{/each}
 	</div>
+</div>
+
+<div class="div4">
+	<p class="text-center text-xl align-middle"><b>Any offer present will be shown here</b></p>
 </div>
